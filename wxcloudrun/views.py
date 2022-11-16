3f2c383a8d4f542347e1cbeacd6027401ed533b8
goodgqbb <101711628+goodgqbb@users.noreply.github.com>
2022-11-16 09:24:38 +0800

    Update views.py

--- wxcloudrun/views.py
+++ wxcloudrun/views.py
@@ -18,31 +18,36 @@ from tencentcloud.common.profile.http_profile import HttpProfile
 from tencentcloud.common.exception.tencent_cloud_sdk_exception import TencentCloudSDKException
 from tencentcloud.nlp.v20190408 import nlp_client, models
 
+API_KEY = "3bZkFHFfLOUg091GPgRkeOVk"
+SECRET_KEY = "V8P7KmzmvKSKWt4abbLLo1RmbqoC0qTz"
+def get_access_token():
+    """
+    使用 AK，SK 生成鉴权签名（Access Token）
+    :return: access_token，或是None(如果错误)
+    """
+    url = "https://aip.baidubce.com/oauth/2.0/token"
+    params = {"grant_type": "client_credentials", "client_id": API_KEY, "client_secret": SECRET_KEY}
+    return str(requests.post(url, params=params).json().get("access_token"))
 
 @app.route('/', methods=['POST'])
 def upload():
     
     text = request.json.get('text')
     try:
-        cred = credential.Credential("3bZkFHFfLOUg091GPgRkeOVk", "V8P7KmzmvKSKWt4abbLLo1RmbqoC0qTz")
-        httpProfile = HttpProfile()
-        httpProfile.endpoint = "nlp.tencentcloudapi.com"
-
-        clientProfile = ClientProfile()
-        clientProfile.httpProfile = httpProfile
-        client = nlp_client.NlpClient(cred, "ap-guangzhou", clientProfile)
-
-        req = models.SentimentAnalysisRequest()
-        params = {
-            "Text": text,
-            "Mode": "2class"
+        url = "https://aip.baidubce.com/rpc/2.0/nlp/v1/sentiment_classify?charset=UTF-8&access_token=" + get_access_token()
+        payload = json.dumps({
+            "text": text
+        })
+         headers = {
+            'Content-Type': 'application/json',
+            'Accept': '/json'
         }
-        req.from_json_string(json.dumps(params))
 
-        resp = client.SentimentAnalysis(req)
-        if 'positive' in resp.to_json_string():
+        response = requests.request("POST", url, headers=headers, data=payload)
+        response = json.loads(json.dumps(response.json()))
+        if response['items'][0]['positive_prob']>0:
             return json.dumps(1, ensure_ascii=False)
-        else:
+        else :
             return json.dumps(0, ensure_ascii=False)
 
     except TencentCloudSDKException as err:
